18b5b56f1e4872249021be97bb2798007cbc82b8
AK7795 <akshaysingodi@gmail.com>
2022-03-16 15:16:39 +0530

    assign

---
+++ src/prct.java
@@ -0,0 +1,12 @@
+public class prct {
+    public static void main(String[] args) {
+        try {
+            int a=0,b =0,c;
+        c = (a*2)/b;
+        }
+        catch (Exception x){
+            System.out.println(x);
+
+        }
+    }
+}
